57fe820fd3147cac6a642f88663530e19df54ce3
顏淩育 <louis@conquers.co>
2021-08-09 16:45:06 +0800

    異動 host="0.0.0.0"

--- app/main.py
+++ app/main.py
@@ -375,4 +375,4 @@ def put_repair_info_end_time(
 
 
 if __name__ == '__main__':
-    uvicorn.run('main:app', host="127.0.0.1", port=5000, log_level="info", reload=True, debug=True, workers=1)
+    uvicorn.run('main:app', host="0.0.0.0", port=5000, log_level="info", reload=True, debug=True, workers=1)
