d8ee306edecc76fd0d4ec737ddac135fc6b31509
顏淩育 <louis@conquers.co>
2021-08-15 10:30:31 +0800

    test CORS

--- app/main.py
+++ app/main.py
@@ -11,15 +11,16 @@ app = FastAPI()
 
 app.debug = True
 origins = [
-    "*",
+    "http://localhost",
+    "http://localhost:8080"
 ]
 
 app.add_middleware(
     CORSMiddleware,
     allow_origins=origins,
-    # allow_credentials=True,
-    # allow_methods=["*"],
-    # allow_headers=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
 )
 
 
