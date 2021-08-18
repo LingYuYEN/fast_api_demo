c330839f4fea7a93417eedea1eaaf472bdbc5a98
顏淩育 <louis@conquers.co>
2021-08-18 13:41:23 +0800

    修正 jsonfile 路徑

--- app/access_jsonfile.py
+++ app/access_jsonfile.py
@@ -1,12 +1,14 @@
 import json
 import os
+from pathlib import Path
 
 dict_list = []
+jsonfile_name = Path('fast_api_demo/jsonfile.json')
 
 
 def write_jsonfile(dic):
-    if os.path.exists('jsonfile.json'):
-        with open('jsonfile.json', 'r+') as jsonfile_list:  # 如果 json 檔案存在，就載入
+    if os.path.exists(jsonfile_name):
+        with open(jsonfile_name, 'r+') as jsonfile_list:  # 如果 json 檔案存在，就載入
             dic_list = json.load(jsonfile_list)  # 並轉換成 dic_list
             dic_list.append(dic)  # 將 dic append 到 dic_list
             jsonfile_list.seek(0)  # 指定光標位置，避免將 list 加到原本 jsonfile_list 後
@@ -14,7 +16,7 @@ def write_jsonfile(dic):
             jsonfile_list.write(json_obj_list)
             jsonfile_list.close()
     else:
-        with open('jsonfile.json', 'w') as jsonfile:  # 如果沒有 json 檔案，就新增
+        with open(jsonfile_name, 'w') as jsonfile:  # 如果沒有 json 檔案，就新增
             dict_list.append(dic)  # 先將 dict 加入陣列
             json_object_list = json.dumps(dict_list, indent=4)  # 再將 dict_list 轉成 json
             jsonfile.write(json_object_list)  # 並寫入
@@ -22,8 +24,8 @@ def write_jsonfile(dic):
 
 
 def load_jsonfile():
-    if os.path.exists('jsonfile.json'):
-        with open('jsonfile.json', 'r') as jsonfile_list:
+    if os.path.exists(jsonfile_name):
+        with open(jsonfile_name, 'r') as jsonfile_list:
             dict_data_list = json.load(jsonfile_list)  # 轉換成 dic_list
             return dict_data_list
     else:
