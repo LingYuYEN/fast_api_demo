e2a26778f1d5ce8de9009eb33d74bd2c1227969e
顏淩育 <louis@conquers.co>
2021-08-20 20:48:10 +0800

    find jsonfile path

--- app/access_jsonfile.py
+++ app/access_jsonfile.py
@@ -3,7 +3,7 @@ import os
 
 dict_list = []
 repair_record_dict_list = []
-jsonfile_name = "jsonfile.json"
+jsonfile_name = "/root/fast_api_demo/jsonfile.json"
 
 exe = 'jsonfile.json'
 # if the exe just in current dir
