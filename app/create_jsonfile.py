7cff0b2437d0e53017d96a996a613f87a6f53928
顏淩育 <louis@conquers.co>
2021-08-20 20:53:58 +0800

    fix jsonfile path

--- app/create_jsonfile.py
+++ app/create_jsonfile.py
@@ -1,18 +1,18 @@
 import json
 import os
 
-jsonfile_name = "/root/fast_api_demo/jsonfile.json"
+# jsonfile_name = "/root/fast_api_demo/jsonfile.json"
 # jsonfile_name = "jsonfile_test.json"
 
 
-json_str = '{"a": "a"}'
-
-
-def creat_jsonfile():
-    with open(jsonfile_name, 'w') as jsonfile:
-        jsonfile.write(json_str)
-        print(jsonfile)
-        jsonfile.close()
+# json_str = '{"a": "a"}'
+#
+#
+# def creat_jsonfile():
+#     with open(jsonfile_name, 'w') as jsonfile:
+#         jsonfile.write(json_str)
+#         print(jsonfile)
+#         jsonfile.close()
 
 
 # creat_jsonfile()
