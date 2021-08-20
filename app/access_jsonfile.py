6be666ad2ffa057cfa2002d6f6bfff4acc0aa4ab
顏淩育 <louis@conquers.co>
2021-08-20 23:00:01 +0800

    test write json

--- app/access_jsonfile.py
+++ app/access_jsonfile.py
@@ -17,31 +17,24 @@ cwd = os.getcwd()
 print("Current working directory:", cwd)
 
 
-def write_jsonfile():
+def write_jsonfile(dic):
     global dict_list
-    # print(dic)
-    if os.path.exists(jsonfile_name):
-        return
-    else:
-        with open(jsonfile_name, 'w') as jsonfile:
-            jsonfile.write()
-            jsonfile.close()
 
-    # if os.path.exists(jsonfile_name):
-    #     with open(jsonfile_name, 'r+') as jsonfile_list:  # 如果 json 檔案存在，就載入
-    #         dict_list = json.load(jsonfile_list)  # 並轉換成 dic_list
-    #         dict_list.append(dic)  # 將 dic append 到 dic_list
-    #         jsonfile_list.seek(0)  # 指定光標位置，避免將 list 加到原本 jsonfile_list 後
-    #         json_obj_list = json.dumps(dict_list, indent=4)  # 再將 dic_list 轉成 json
-    #         jsonfile_list.write(json_obj_list)
-    #         jsonfile_list.close()
-    # else:
-    #     with open(jsonfile_name, 'w') as jsonfile:  # 如果沒有 json 檔案，就新增
-    #         print(os.path.abspath(jsonfile_name))
-    #         dict_list.append(dic)  # 先將 dict 加入陣列
-    #         json_object_list = json.dumps(dict_list, indent=4)  # 再將 dict_list 轉成 json
-    #         jsonfile.write(json_object_list)  # 並寫入
-    #         jsonfile.close()
+    if os.path.exists(jsonfile_name):
+        with open(jsonfile_name, 'r+') as jsonfile_list:  # 如果 json 檔案存在，就載入
+            dict_list = json.load(jsonfile_list)  # 並轉換成 dic_list
+            dict_list.append(dic)  # 將 dic append 到 dic_list
+            jsonfile_list.seek(0)  # 指定光標位置，避免將 list 加到原本 jsonfile_list 後
+            json_obj_list = json.dumps(dict_list, indent=4)  # 再將 dic_list 轉成 json
+            jsonfile_list.write(json_obj_list)
+            jsonfile_list.close()
+    else:
+        with open(jsonfile_name, 'w') as jsonfile:  # 如果沒有 json 檔案，就新增
+            print(os.path.abspath(jsonfile_name))
+            dict_list.append(dic)  # 先將 dict 加入陣列
+            json_object_list = json.dumps(dict_list, indent=4)  # 再將 dict_list 轉成 json
+            jsonfile.write(json_object_list)  # 並寫入
+            jsonfile.close()
 
 
 def load_jsonfile():
