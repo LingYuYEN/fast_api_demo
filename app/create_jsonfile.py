9f69bc988a31625ef8450ad79cd591d27413a185
顏淩育 <louis@conquers.co>
2021-08-20 20:44:58 +0800

    find jsonfile path

--- app/create_jsonfile.py
+++ app/create_jsonfile.py
@@ -2,10 +2,17 @@ import json
 import os
 
 jsonfile_name = "./root/fast_api_demo/jsonfile.json"
+# jsonfile_name = "jsonfile_test.json"
+
+
+json_str = '{"a": "a"}'
 
 
 def creat_jsonfile():
     with open(jsonfile_name, 'w') as jsonfile:
+        jsonfile.write(json_str)
         print(jsonfile)
-        return "成功新增 jsonfile"
+        jsonfile.close()
 
+
+creat_jsonfile()
