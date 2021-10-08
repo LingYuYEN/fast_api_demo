d79bb6797eca9e41dc3ce0d2d44ae8c1db742ea3
顏淩育 <louis@conquers.co>
2021-10-08 13:54:45 +0800

    update jsonfile path create mail schedule

--- app/access_jsonfile.py
+++ app/access_jsonfile.py
@@ -3,8 +3,8 @@ import os
 
 # dict_list = []
 # repair_record_dict_list = []
-jsonfile_name = "./jsonfile.json"
-members_jsonfile = "./members.json"
+jsonfile_name = "/root/fast_api_demo/jsonfile.json"
+members_jsonfile = "/root/fast_api_demo/members.json"
 # jsonfile_name = "jsonfile.json"
 
 # if the exe just in current dir
