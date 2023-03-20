1cc471b2f0618423d45a4c3e8909abb10453c4cf
顏淩育 <louis@conquers.co>
2023-03-20 16:09:29 +0800

    create ap add api and optimization model

--- app/json_trans_class.py
+++ app/json_trans_class.py
@@ -40,12 +40,3 @@ def delete_jsonfile():
 write_jsonfile(data)
 load_jsonfile()
 delete_jsonfile()
-# user_a = MemberModel(id=1, account='aaps', password='aaps', priority=1)
-# # user_b = MemberModel(2, 'bbps', 'bbps', 1)
-# # user_c = MemberModel(3, 'ccps', 'ccps', 1)
-# # user_d = MemberModel(4, 'ddps', 'ddps', 1)
-#
-# # user_a = {'id': 1, 'account': 'aaps', 'password': 'aaps', 'priority': 1}
-# user_array = [user_a]
-#
-# print(user_array)
